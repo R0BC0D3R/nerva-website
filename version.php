a234c533388405ad621410e883f1799b42dde1d9
R0BC0D3R <R0BC0D3R@gmail.com>
2021-12-02 19:40:58 -0500

    Update download links to use new versions

--- version.php
+++ version.php
@@ -1,18 +1,18 @@
 <?php 
-define('CLI_VERSION', 'v0.1.7.4');
-define('CLI_CODENAME', 'NoWasp');
-define('GUI_VERSION', 'v0.3.0.0');
+define('CLI_VERSION', 'v0.1.7.5');
+define('CLI_CODENAME', 'Speed');
+define('GUI_VERSION', 'v0.3.1.0');
 define('GUI_CODENAME', '');
 define('LEDGER_VERSION', 'v0.1.7.0+');
 
-define('LINUX_CLI_LINK', 'https://github.com/nerva-project/nerva/releases/download/v0.1.7.4/nerva-v0.1.7.4_linux_minimal.zip');
-define('WINDOWS_CLI_LINK', 'https://github.com/nerva-project/nerva/releases/download/v0.1.7.4/nerva-v0.1.7.4_windows_minimal.zip');
-define('MAC_CLI_LINK', 'https://github.com/nerva-project/nerva/releases/download/v0.1.7.4/nerva-v0.1.7.4_osx_minimal.zip');
+define('LINUX_CLI_LINK', 'https://github.com/nerva-project/nerva/releases/download/v0.1.7.5/nerva-v0.1.7.5_linux_minimal.zip');
+define('WINDOWS_CLI_LINK', 'https://github.com/nerva-project/nerva/releases/download/v0.1.7.5/nerva-v0.1.7.5_windows_minimal.zip');
+define('MAC_CLI_LINK', 'https://github.com/nerva-project/nerva/releases/download/v0.1.7.5/nerva-v0.1.7.5_osx_minimal.zip');
 
-define('LINUX_GUI_LINK', 'https://github.com/nerva-project/nerva-gui/releases/download/v0.3.0.0/nerva-gui-v0.3.0.0_linux-x64.zip');
-define('WINDOWS_GUI_LINK', 'https://github.com/nerva-project/nerva-gui/releases/download/v0.3.0.0/nerva-gui-v0.3.0.0_win-x64.zip');
-define('MAC_GUI_LINK', 'https://github.com/nerva-project/nerva-gui/releases/download/v0.3.0.0/nerva-gui-v0.3.0.0_osx-x64.zip');
+define('LINUX_GUI_LINK', 'https://github.com/nerva-project/nerva-gui/releases/download/v0.3.1.0/nerva-gui-v0.3.1.0_linux-x64.zip');
+define('WINDOWS_GUI_LINK', 'https://github.com/nerva-project/nerva-gui/releases/download/v0.3.1.0/nerva-gui-v0.3.1.0_win-x64.zip');
+define('MAC_GUI_LINK', 'https://github.com/nerva-project/nerva-gui/releases/download/v0.3.1.0/nerva-gui-v0.3.1.0_osx-x64.zip');
 
 define('LEDGER_LINK', 'https://nerva.one/#');
-define('QUICKSYNC_LINK', 'https://github.com/nerva-project/nerva/releases/download/v0.1.7.4/quicksync.raw');
+define('QUICKSYNC_LINK', 'https://github.com/nerva-project/nerva/releases/download/v0.1.7.5/quicksync.raw');
 ?>
