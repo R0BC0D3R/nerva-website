2f5f2d9111c2f0e2302e1fd13eb1e8e8f5c79a68
R0BC0D3R <R0BC0D3R@gmail.com>
2021-12-20 21:26:44 -0500

    Update GUI/QuickSync download links

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php 
 define('CLI_VERSION', 'v0.1.7.5');
 define('CLI_CODENAME', 'Speed');
-define('GUI_VERSION', 'v0.3.1.0');
+define('GUI_VERSION', 'v0.3.2.0');
 define('GUI_CODENAME', '');
 define('LEDGER_VERSION', 'v0.1.7.0+');
 
@@ -9,10 +9,10 @@ define('LINUX_CLI_LINK', 'https://github.com/nerva-project/nerva/releases/downlo
 define('WINDOWS_CLI_LINK', 'https://github.com/nerva-project/nerva/releases/download/v0.1.7.5/nerva-v0.1.7.5_windows_minimal.zip');
 define('MAC_CLI_LINK', 'https://github.com/nerva-project/nerva/releases/download/v0.1.7.5/nerva-v0.1.7.5_osx_minimal.zip');
 
-define('LINUX_GUI_LINK', 'https://github.com/nerva-project/nerva-gui/releases/download/v0.3.1.0/nerva-gui-v0.3.1.0_linux-x64.zip');
-define('WINDOWS_GUI_LINK', 'https://github.com/nerva-project/nerva-gui/releases/download/v0.3.1.0/nerva-gui-v0.3.1.0_win-x64.zip');
-define('MAC_GUI_LINK', 'https://github.com/nerva-project/nerva-gui/releases/download/v0.3.1.0/nerva-gui-v0.3.1.0_osx-x64.zip');
+define('LINUX_GUI_LINK', 'https://github.com/nerva-project/nerva-gui/releases/download/v0.3.2.0/nerva-gui-v0.3.2.0_linux-x64.zip');
+define('WINDOWS_GUI_LINK', 'https://github.com/nerva-project/nerva-gui/releases/download/v0.3.2.0/nerva-gui-v0.3.2.0_win-x64.zip');
+define('MAC_GUI_LINK', 'https://github.com/nerva-project/nerva-gui/releases/download/v0.3.2.0/nerva-gui-v0.3.2.0_osx-x64.zip');
 
 define('LEDGER_LINK', 'https://nerva.one/#');
-define('QUICKSYNC_LINK', 'https://github.com/nerva-project/nerva/releases/download/v0.1.7.5/quicksync.raw');
+define('QUICKSYNC_LINK', 'https://nerva.one/quicksync/quicksync.raw');
 ?>
