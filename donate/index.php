9835ec4d9a016ca0a0bfb29b0c9a848a1d687512
R0BC0D3R <R0BC0D3R@gmail.com>
2022-01-19 13:21:05 -0500

    Add donation page

---
+++ donate/index.php
@@ -0,0 +1,188 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Google Analytics Begins - Global site tag -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-J46F6TCMGV"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-J46F6TCMGV');
+    </script> 
+    <!-- Google Analytics Ends -->
+
+    <title>Nerva - Advanced Ideas. Simple Hardware.</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="NERVA (XNV) Donation page.">
+    <meta name="keywords" content="nerva, xnv, crypto, cryptocurrency, crypto currency, one cpu one vote, mining, crypto mining, wallet, nerva wallet, crypto wallet">
+
+    <link rel="dns-prefetch" href="//fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+        integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/owl.carousel.min.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+    <link href="../css/style.css" rel="stylesheet">
+    <!-- jQuery and Bootstrap -->
+    <script src="../js/jquery-3.2.1.min.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <!-- Plugins JS -->
+    <script src="../js/owl.carousel.min.js"></script>
+    <!-- Custom JS -->
+    <script src="../js/script.js"></script>
+    <script src="../js/paperwallet/languages.js"></script>
+</head>
+
+<body data-spy="scroll" data-target="#navbar" data-offset="30">
+
+<!-- Navigation Begins -->
+    <div class="nav-menu fixed-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar-dark navbar-expand-xl">
+                        <a class="navbar-brand" href="#">
+                            <img class="img-fluid nerva-logo-icon-white" alt="nerva-logo-white"
+                                src="../images/png-nerva-logo-white-256x256.png">
+                            <img src="../images/logo.png" class="img-fluid" alt="logo">
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar"
+                            aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbar">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item"> <a class="nav-link" href="https://nerva.one/">HOME <span class="sr-only">(current)</span></a> </li>                      
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- Navigation Ends -->
+
+
+<!-- Home Begins -->
+    <header class="bg-gradient" id="home">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-md-6 d-flex align-items-center">
+                    <img src="../images/nerva-crowdgift-1.png" alt="nerva-chain" class="img-fluid">
+                </div>
+                <div class="col-md-6 d-flex align-items-center">
+                    <div>
+                        <div class="spacer hidden-xs"></div>
+                        <h1 class="page-intro">
+                            Donate to Support NERVA Project
+                        </h1>
+                        <br>
+                        <p class="mb-4 white-text">
+                            NERVA (XNV) is a private and secure cryptocurrency that you can mine on your computer
+                        </p>
+                        <p class="mb-4 white-text">
+                            It is community managed and driven on a voluntary basis.  With your support we can keep pushing forward, 
+                                potentially buy back XNV and use the funds for future developments or marketing campaigns.
+                        </p>       
+                    </div>
+                </div>
+            </div>
+            <div class="spacer"></div>
+        </div>
+    </header>
+<!-- Home Begins -->
+
+
+<!-- Donate Begins -->
+<div class="section light-bg" id="satoshi-wp">
+        <div class="container">
+			<div class="section-title">
+				<div class="spacer"></div>
+				<h2>How to Donate</h2>
+				<p>
+                    If you'd like to make a donation to NERVA Project (every little bit counts), please send your donation to one of the following addresses:
+                    <br /><br />
+                    <b>NERVA (XNV) Deposit Address:</b>
+                    <br />
+                    NizKdaicW4bVfYB3AVhnsq9qnvUYSKe568YaNV2KQCYCDrNGzpvxqBo6mxF8cBkiQDU5xkgB2PrUGFKf66wVDVoNbQBhtSXbAUg9VyRtP7Xhp
+                    <br /><br />
+                    <b>Bitcoin (BTC) Treasury Deposit Address:</b>
+                    <br />
+                    bc1qk8aafmjcxeewzw4dcgkdc2cwegsqxeh5krndgrat45ux97kqpu6s3vth8t
+                    <br /><br />
+                    <b>Litecoin (LTC) Deposit Address:</b>
+                    <br />
+                    LfFN4x4WFr1pDfW2FBjLEjN2w1SVWai7e5               
+				</p>				
+			</div>
+	    </div>	
+    </div>
+<!-- Donate Ends -->
+
+
+<!-- Treasury Begins -->
+	<div class="section" id="satoshi-wp">
+        <div class="container">
+			<div class="section-title">
+				<div class="spacer"></div>
+				<h2>Current Treasury Holdings</h2>
+				<p>
+                    As a community project, we like to be transparent about our financials. Here is what our treasury accounts currently hold:
+                    <br /><br />
+                    XNV Deposit Address: 100100
+                    <br /><br />
+                    <a href="https://www.blockchain.com/btc/address/bc1qk8aafmjcxeewzw4dcgkdc2cwegsqxeh5krndgrat45ux97kqpu6s3vth8t" target="_blank">BTC Treasury Deposit Address</a>: 0
+                    <br /><br />
+                    LTC Deposit Address: 0
+                    <br /><br />
+                    The treasury account is held save by long-term community members and will only be used for activities that have been discussed and agreed upon by the community in advance.
+				</p>				
+			</div>
+	    </div>	
+    </div>
+<!-- Treasury Ends -->
+
+
+<!-- January Giveaway Begins -->
+<div class="section light-bg" id="satoshi-wp">
+        <div class="container">
+			<div class="section-title">
+				<div class="spacer"></div>
+				<h2>January 2022 Meme Giveaway Campaign</h2>
+				<p>
+                    As a fun little experiment, we are giving away NFTs of some of our phantastic NERVA memes to the 10 community members who make the highest donations <b>between 20 January and 19 February 2022</b>.
+                    <br /><br />
+                    Have a look at our collectibles up for grabs on OpenSea: [link, tbd]
+                    <br /><br />
+                    In order to qualify, just make your donation and send a DM to the account who made the respective announcement on Discord, Twitter or Reddit. 
+                    Don't forget to state the amount you sent and provide transaction details (just give us the exact date and time in case you don't have a transaction hash).
+                    The top 10 contributors will be notified after 19 February 2022 and will be able to pick their NFT in descending order of their contribution (i.e. winner #1 choses first).
+                    Note: All NFTs are set up on the Polygon blockchain, so there will be no fees for receiving them.
+                    <br /><br />
+                    See our announcements on:
+                    <br />
+                    Discord: [link]
+                    <br />
+                    Twitter: [link]
+                    <br />
+                    Reddit: [link]                   
+				</p>				
+			</div>
+	    </div>	
+    </div>
+<!-- January Giveaway Ends -->
+
+
+    <footer class="my-5 text-center">
+        <small>
+            <div class="spacer"></div>
+            <p>Copyright © 2018 - 2022 <a href="https://nerva.one/">NERVA Project</a></p>
+        </small>
+    </footer>
+</body>
+
+</html>
